839ac34573ebdb78313b65493f708ae1d157e41a
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2026-02-03 09:44:08 -0800

    simhash + extract_text defrag

--- scraper.py
+++ scraper.py
@@ -1,15 +1,17 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin, urldefrag
 from typing import Iterable, Tuple
 from bs4 import BeautifulSoup
+import hashlib
 
 
 MAX_HTML_BYTES = 5000000
 MAX_SIGNATURE_REPEATS = 10
 MIN_WORDS = 50
+HAMMING_THRESH = 5
 
-# To count the signature for similarity of pages
-signature_counts = {}
+# To store the signature for similarity of pages we have already accepted
+seen_signature = []
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -28,32 +30,87 @@ def extract_next_links(url, resp):
     raw_response = resp.raw_response
     if not raw_response: 
         return []
+    
     html = raw_response.content
     if not html: 
         return []
-    soup = BeautifulSoup(html, 'lxml')
+    
+    if len(html) > MAX_HTML_BYTES: # Determine very large files
+        return []
+
     urls = []
-    for link in soup.find_all('a'):
-        # TODO: defragment url
-        urls.append(link.get('href'))
-        # print(link.get('href'))
-
-    # if not html:
-    #     return []
     
-    # if len(html) > MAX_HTML_BYTES: # Determine very large files
-    #     return []
-    
-    # word_count = len(words)   # To determine the information
+    words = []
+    urls = []
+    seen_links = set()
 
-    # signature = " ".join(words[:200])
-    # if similarity_compare(signature): # Build similarity signature/report
-    #     return []               
+    base = getattr(resp, "url", None) or url #handles redirects
+    for tok, kind in extract_text(html, base):
+        if kind == "word":
+            words.append(tok)
+        elif kind == "URL":
+            if tok not in seen_links:
+                seen_links.add(tok)
+                urls.append(tok)
 
-    # if word_count < MIN_WORDS:
-    #     return []
+    # Generate a 64-bit signature for this page using SimHash
+    page_val = page_signature(words, k=5)
+    if too_similar(page_val):
+        return []          
+
+    if len(words) < MIN_WORDS:
+        return []
     
     return urls
+
+def make_shingles(words, k = 5):
+    # Generate k-word shingles (sliding windows) from token list.
+    if len(words) < k:
+        return
+
+    for i in range(len(words) - k + 1):
+        yield " ".join(words[i:i+k])
+
+def encode_shingle(words):
+    # Uses hashlib.blake2b hashfunction to encode shingle into 64 bit int
+    h = hashlib.blake2b(words.encode("utf-8"), digest_size=8).digest()
+    return int.from_bytes(h, byteorder="big", signed=False)
+
+def bit_difference(a, b):
+    # Compute distance between two 64-bit integers.
+    return (a ^ b).bit_count()
+
+
+def page_signature(words, k = 5) -> int:
+    # initialize a 64-dimension vote vector
+
+    v = [0] * 64
+    for sh in make_shingles(words, k):
+        h = encode_shingle(sh)  # 64-bit int
+        for i in range(64):
+            if (h >> i) & 1:
+                #if bit == 1: add vote
+                v[i] += 1
+            else:
+                #if bit == 0: remove vote
+                v[i] -= 1
+    fp = 0
+    for i in range(64):
+        if v[i] >= 0:
+            # If more positive votes than negative, set bit to 
+            fp |= (1 << i)
+    
+    return fp
+
+def too_similar(fp: int) -> bool:
+    # Compare this signatures against all previously seen signatures.
+    # Returns True if any are within HAMMING_THRESH.
+    for old in seen_signature:
+        if bit_difference(fp, old) <= HAMMING_THRESH:
+            return True
+    seen_signature.append(fp)
+    return False
+
 
 # O(n) where n is the length of the token string
 def format_alphanum(token):
@@ -66,13 +123,15 @@ def format_alphanum(token):
     formatted_token.append(token[current+1:].lower())
     return formatted_token
 
-def extract_text(html: bytes) -> Iterable[Tuple[str, str]]:
+def extract_text(html: bytes, url: str) -> Iterable[Tuple[str, str]]:
     # creates a BeautifulSoup object that helps parse html beautifully
     # make sure to run {pip install beautifulsoup4}
     parser = BeautifulSoup(html, "html.parser")
     # we parse through each item in the html
 
     do_not_parse = {'style', 'title', '[document]', 'script', 'meta', 'head'}
+
+    url, _ = urldefrag(url)
 
     for item in parser.body.find_all(True):
         # ensures that we DO NOT PARSE through potential style objects or javascript
@@ -81,7 +140,20 @@ def extract_text(html: bytes) -> Iterable[Tuple[str, str]]:
         # we will only parse text from the parent once bc of recursive=False
         text = "".join(item.find_all(string=True, recursive=False)).strip()
         if (item.name == 'a' and item.get("href")):
-            yield item.get("href"), "URL"
+            href = item.get("href")
+            if href:
+                href = href.strip()
+
+                low = href.lower()
+                # skip non-crawl schemes
+                if low.startswith(("mailto:", "javascript:", "tel:")):
+                    href = None
+
+            if href:
+                abs_url = urljoin(url, href)     # make absolute
+                abs_url, _ = urldefrag(abs_url)       
+                yield abs_url, "URL"
+
         if (text):
             # split text into tokens (split on whitespace)
             tokens = format_alphanum(text)
@@ -91,19 +163,8 @@ def extract_text(html: bytes) -> Iterable[Tuple[str, str]]:
                     yield token, "word"
     # Parses the html
     # Yields a stream of tokens of either words or URLS with an identifier constructed as Tuple
-    # EX: ("hello", "word"), ("www..ics.uci.edu/", "URL")
+    # EX: ("hello", "word"), ("www.ics.uci.edu/", "URL")
     
-
-def similarity_compare(signature: str)-> bool:
-    # Stores signatures of Pages into a Dictionary
-    # If signature count reaches threshold, Don't extract URL from page
-    if signature in signature_counts:
-        signature_counts[signature] += 1
-    else:
-        signature_counts[signature] = 1
-    
-    return signature_counts[signature] > MAX_SIGNATURE_REPEATS
-
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
